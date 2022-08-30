36d3d5b865b51922549c732f4be967603f496b11
QQWQAQWQQ <112449389+QQWQAQWQQ@users.noreply.github.com>
2022-08-30 20:54:04 +0800

    Aircraft Battle

---
+++ Aircraft Battle/src/飞机大战/Item001.java
@@ -0,0 +1,29 @@
+package 飞机大战;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class Item001 extends Item{
+    public Item001(MyPanel myPanel) {
+        super(myPanel);
+        // TODO Auto-generated constructor stub
+        this.width=30;
+        this.height=30;
+        this.x=20;
+        this.y=50;
+        this.images=new Image[] {
+                Toolkit.getDefaultToolkit().getImage("star01.png"),
+                Toolkit.getDefaultToolkit().getImage("star02.png"),
+                Toolkit.getDefaultToolkit().getImage("star03.png"),
+                Toolkit.getDefaultToolkit().getImage("star04.png"),
+                Toolkit.getDefaultToolkit().getImage("star05.png"),
+                Toolkit.getDefaultToolkit().getImage("star06.png"),
+                Toolkit.getDefaultToolkit().getImage("star07.png"),
+                Toolkit.getDefaultToolkit().getImage("star08.png"),
+                Toolkit.getDefaultToolkit().getImage("star09.png"),
+        };
+        this.imageSpeed=(int)(Math.random()*20+5);
+        this.speed=3;
+    }
+
+}
---
+++ Aircraft Battle/src/飞机大战/Main.java
@@ -0,0 +1,9 @@
+package 飞机大战;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new BaseFrame();
+    }
+
+}
